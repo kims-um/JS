74815d0f48ab99fef34d0de130b5d33ef609f937
kims-um <71931367+kims-um@users.noreply.github.com>
2021-06-28 12:46:54 +0900

    parse Int,Float

--- JS.js
+++ JS.js
@@ -1,2 +1,8 @@
-var name = prompt();
-console.log(name, "님 환영합니다");
+var height = prompt("몸무게를 입력해주세요");
+console.log(height, typeof height);
+
+var height_int = parseInt(height);
+console.log(height_int, typeof height_int);
+
+var height_float = parseFloat(height);
+console.log(height_float, typeof height_float);
